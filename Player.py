46013f47029b58143b94101521beeadcc2a0e658
datasink <dshalfait@gmail.com>
2023-03-22 14:23:35 +0100

    add attribute name to Player + init func

--- Player.py
+++ Player.py
@@ -4,8 +4,12 @@ from Piece import Piece
 
 class Player:
     color: Color = None
+    name: str
     score: int = 0
     pieces: list[Piece]
+
+    def __init__(self, name: str):
+        self.name = name
 
 
 
